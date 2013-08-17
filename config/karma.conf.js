f4d132802e0a4a0b02d0bc9a39d8df9c2aeaa01c
Eric Guo <eric.guo@sandisk.com>
2013-08-17 14:04:50 +0800

    Upgrade karma.conf.js to version 0.10.1 format

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,21 +1,39 @@
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    basePath : '../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  // 'app/js/**/*.js',
-  'app/lessons/07/**/*.js'//,
-  // 'test/unit/**/*.js'
-];
+    frameworks : ["jasmine"],
 
-autoWatch = true;
+    files : [
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-*.js',
+      'test/lib/angular/angular-mocks.js',
+      // 'app/js/**/*.js',
+      'app/lessons/07/**/*.js'//,
+      // 'test/unit/**/*.js'
+    ],
 
-browsers = ['Chrome'];
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch : true,
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
-};
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers : ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun : true,
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  })
+}
